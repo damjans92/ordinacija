46d1fa4d9b9c78aa4873aa036babe356e92dbee2
Damjan Savkovic <damjans92@gmail.com>
2022-04-22 22:52:24 +0200

    Added google tag manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,20 @@ module.exports = {
     //   },
     // },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-199943429-1", // Google Analytics / GA
+        ],
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-plugin-page-creator`,
       options: {
         path: `${__dirname}/src/pages`,
